d15d63f6e513e314d0e2f116eb6df687705e5c0f
Jack Whelan <jackw97@live.ie>
2019-06-18 15:16:37 +0100

    Created the navigation bar plus a submenu (acc)

--- Control.py
+++ Control.py
@@ -7,6 +7,33 @@ class Statusbar(Frame):
 		self.parent = parent
 		status = Label(root, text="Idle", bd=1, relief=SUNKEN, anchor=W)
 		status.pack(side=BOTTOM, fill=X)
+		
+		
+class Navbar(Frame):
+	def __init__(self, parent, *args, **kwargs):
+		Frame.__init__(self, parent, *args, **kwargs)
+		self.parent = parent
+		
+		# ***** Navigation bar functionality *****
+		# Account > Login
+		def nav_login():
+			print("Login button clicked.")
+			
+		# Account > Register
+		def nav_register():
+			print("Register button clicked.")
+		
+		# ***** Navigation bar layout *****
+		menu = Menu(root)
+		root.config(menu=menu)
+		
+		# Account menu option
+		accountMenu = Menu(menu, tearoff=0)
+		accountMenu.add_command(label="Login", command=nav_login)
+		accountMenu.add_command(label="Register", command=nav_register)
+		
+		# Adding menu options to navigation menus
+		menu.add_cascade(label="Account", menu=accountMenu)
 
 
 class GUI(Frame):
@@ -14,6 +41,7 @@ class GUI(Frame):
 		Frame.__init__(self, parent, *args, **kwargs)
 		self.parent = parent
 		self.statusbar = Statusbar(self)
+		self.navbar = Navbar(self)
 		self.statusbar.pack()
 
 
