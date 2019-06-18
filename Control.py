05644334a08edd35a3cacc2fd595e70343f9e4d6
Jack Whelan <jackw97@live.ie>
2019-06-18 14:53:48 +0100

    Created the status bar class and packed it.

--- Control.py
+++ Control.py
@@ -1,9 +1,21 @@
 from tkinter import *
 
+
+class Statusbar(Frame):
+	def __init__(self, parent, *args, **kwargs):
+		Frame.__init__(self, parent, *args, **kwargs)
+		self.parent = parent
+		status = Label(root, text="Idle", bd=1, relief=SUNKEN, anchor=W)
+		status.pack(side=BOTTOM, fill=X)
+
+
 class GUI(Frame):
 	def __init__(self, parent, *args, **kwargs):
 		Frame.__init__(self, parent, *args, **kwargs)
 		self.parent = parent
+		self.statusbar = Statusbar(self)
+		self.statusbar.pack()
+
 
 if __name__ == "__main__":
 	root = Tk()
