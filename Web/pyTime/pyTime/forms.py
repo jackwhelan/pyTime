af5bf0cc5b5ccfefc5463344248bf1679ceea70e
jackwhelan <jackw97@live.ie>
2019-06-23 22:02:06 +0100

    Added Login Form

--- Web/pyTime/pyTime/forms.py
+++ Web/pyTime/pyTime/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, Boolean
 from wtforms.validators import DataRequired, Length, Email
 
 class RegistrationForm(FlaskForm):
@@ -8,3 +8,10 @@ class RegistrationForm(FlaskForm):
 	self.password = PasswordField('Password', validators=[DataRequired()])
 	self.confirm_password = PasswordField('Password', validators=[DataRequired()])
 	self.submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+	self.email = StringField('Email', validators=[DataRequired(), Length(min=2,max=20), Email()])
+	self.password = PasswordField('Password', validators=[DataRequired()])
+	self.remember = BooleanField('Remember Me')
+	self.submit = SubmitField('Login')
